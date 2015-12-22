b750a8122d2d3566e0aef61b464521dfc283f694
Bevan Davis <bevan@pcworkz.co.nz>
2015-12-22 16:04:12 +1300

    Path updates

--- Templates/SharePointFarm/deploy-dc.ps1
+++ Templates/SharePointFarm/deploy-dc.ps1
@@ -6,4 +6,4 @@ Get-AzureRmSubscription -SubscriptionName "Visual Studio Enterprise with MSDN" |
 
 $resourcegroup = Get-AzureRmResourceGroup -Location "Australia East" -Name $resourcegroupname
 
-New-AzureRmResourceGroupDeployment -Name $deploymentname -ResourceGroupName $resourcegroupname -TemplateUri 'OneDrive - Sysdoc NZ\Azure\Templates\SharePointFarm\dc.json'
+New-AzureRmResourceGroupDeployment -Name $deploymentname -ResourceGroupName $resourcegroupname -TemplateUri 'https://raw.githubusercontent.com/beavisnz/azure-resources/master/Templates/SharePointFarm/dc.json'
